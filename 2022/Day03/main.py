0d60edd9924f230bd82be3fd03722ef04ce8aafe
Rynoster <Ryno.Meiboom@gmail.com>
2022-12-04 15:27:07 +0100

    added pytho day 3

--- 2022/Day03/main.py
+++ 2022/Day03/main.py
@@ -19,22 +19,23 @@ def ex02(lines):
     team = []
     prioritySum = 0
     for i, line in enumerate(lines):
+        if i % 3 == 0:
+            team.clear()
         team.append(line)
-        if i > 0 and i % 3 == 0:
+        if i > 0 and i % 3 == 2:
             common = set(team[0]) & set(team[1]) & set(team[2])
             for item in common:
-                if item.islower():
+                if item != '\n' and item.islower():
                     prioritySum += ord(item) - ord('a') + 1
                 else:
                     prioritySum += ord(item) - ord('A') + 27
-                team.clear()
 
     print("ex02: ", prioritySum)
 
 
 def __main__():
     input = open("input.txt", 'r')
-    lines = input.readlines()
+    lines = input.read().splitlines()
     ex01(lines)
     ex02(lines)
 
