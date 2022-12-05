bfbb0d7c3118fdbf6c1d251b7453326f04d38ce2
Ryno95 <rmeiboom@student.codam.nl>
2022-12-05 17:39:05 +0100

    added cpp solution

---
+++ 2022/Day05/main.cpp
@@ -0,0 +1,167 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+void ex01()
+{
+
+}
+
+void ex02()
+{
+
+}
+
+typedef struct s_command
+{
+	int amount;
+	int from;
+	int to;
+
+	void printCommand() {cout<< "A: " << amount << " F: " << from << " T: " << to << endl;}
+}	t_command;
+
+t_command parseCommand(string &line)
+{
+	t_command 	command;
+	vector<string> splitStrings;
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		int spacePos = line.find(32, i);
+		if (spacePos == string::npos)
+			spacePos = line.size();
+		string str = line.substr(i, spacePos - i);
+		if (std::all_of(str.begin(), str.end(), ::isdigit))
+			splitStrings.push_back(str);
+		i = spacePos;
+	}
+
+	command.amount = stoi(splitStrings[0]);
+	command.from = stoi(splitStrings[1]) - 1;
+	command.to = stoi(splitStrings[2]) - 1;
+
+	// command.printCommand();
+
+	return command;
+}
+
+int ex01(vector<t_command> &commands, vector<char> *stack)
+{
+	const int 	size = commands.size();
+	int			count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		int from = commands[i].from;
+		int to = commands[i].to;
+		int amount = commands[i].amount;
+		for (int j = 0; j < amount && !stack[from].empty(); ++j)
+		{
+			stack[to].push_back(stack[from].back());
+			stack[from].pop_back();
+		}
+	}
+	return count;
+}
+
+int ex02(vector<t_command> &commands, vector<char> *stack)
+{
+	const int 	size = commands.size();
+	int			count = 0;
+
+	vector<char> tmp;
+	for (int i = 0; i < size; i++)
+	{
+		int from = commands[i].from;
+		int to = commands[i].to;
+		int amount = commands[i].amount;
+		tmp.clear();
+		for (int j = 0; j < amount && !stack[from].empty(); ++j)
+		{
+			tmp.push_back(stack[from].back());
+			stack[from].pop_back();
+		}
+		while(tmp.size() > 0)
+		{
+			stack[to].push_back(tmp.back());
+			tmp.pop_back();
+		}
+			
+	}
+	return count;
+}
+
+vector<t_command> parse()
+{
+	fstream 				inputFile;
+	stringstream 			buffer;
+	vector<t_command>		commands;
+	inputFile.open("input.txt", ios::in);
+	buffer << inputFile.rdbuf();
+
+	for(string line; getline(buffer, line);)
+	{
+		t_command command = parseCommand(line);
+		commands.push_back(command);
+	}
+	return commands;
+}
+
+void printStack(vector<char> *stacks, int amountOfStack)
+{
+	for (int i = 0; i < amountOfStack; ++i)
+	{
+		std::cout << "stack " << i + 1 << ": ";
+		for(int j = 0; j < stacks[i].size(); ++j)
+		{
+			std::cout << stacks[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main(void)
+{
+	stringstream solution;
+	const int stackSize = 9;
+	// vector<char> stacks[stackSize]{
+	// 	{'Z', 'N'},
+	// 	{'M', 'C', 'D'},
+	// 	{'P'}
+	// };
+
+	std::vector<char> stacks[stackSize]{
+		{'G','F','V','H','P','S'},
+		{'G','J','F','B','V','D','Z','M'},
+		{'G','M','L','J','N'},
+		{'N','G','Z','V','D','W','P'},
+		{'V','R','C','B'},
+		{'V','R','S','M','P','W','L','Z'},
+		{'T','H','P'},
+		{'Q','R','S','N','C','H','Z','V'},
+		{'F','L','G','P','V','Q','J'}
+	};
+	
+	vector<t_command> commands = parse();
+	cout << "After Parse" << endl;
+	ex02(commands, stacks);
+	cout << "After RunCommands" << endl;
+	printStack(stacks, stackSize);
+	for (size_t i = 0; i < stackSize; i++)
+	{
+		if (stacks[i].size() > 0)
+			cout << stacks[i].back();
+	}
+	cout << endl;
+	// cout << solution.str() << endl;
+	// ex01(elfs);
+	// ex02(elfs);
+
+	return 0;
+}
